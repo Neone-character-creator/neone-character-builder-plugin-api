bf11a72163f601f06be3a30e5e118f23e279d8d5
Damie <damienmarble@gmail.com>
2018-09-02 18:47:31 -0400

    Changed generic type.

--- src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginManager.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginManager.java
@@ -9,7 +9,7 @@ import java.util.Optional;
  * Service interface for managing GamePlugins
  * Created by Damien on 11/29/2015.
  */
-public interface PluginManager<T extends GamePlugin<S>, S extends Character>{
+public interface PluginManager<T extends GamePlugin<? extends Character>>{
     Optional<T> getPlugin(String author, String game, String version);
 
     Collection<PluginDescription> getAllPluginDescriptions();
