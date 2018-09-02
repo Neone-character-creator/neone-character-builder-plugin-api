4a42bc48b65963b97bb0f369d941ef29114e0ca5
Damie <damienmarble@gmail.com>
2018-09-02 18:41:05 -0400

    Made PluginManager generic.

--- src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginManager.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginManager.java
@@ -9,12 +9,12 @@ import java.util.Optional;
  * Service interface for managing GamePlugins
  * Created by Damien on 11/29/2015.
  */
-public interface PluginManager{
-    Optional<GamePlugin> getPlugin(String author, String game, String version);
+public interface PluginManager<T extends GamePlugin<S>, S extends Character>{
+    Optional<T> getPlugin(String author, String game, String version);
 
     Collection<PluginDescription> getAllPluginDescriptions();
 
-    Optional<GamePlugin> getPlugin(PluginDescription pluginDescription);
+    Optional<T> getPlugin(PluginDescription pluginDescription);
 
     URI getPluginResource(PluginDescription incomingPluginDescription, String resourceName);
 }
